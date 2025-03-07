49a4a0b1ebf7c62701e092c632aecef120858286
KanHar <knaan.harpaz@gmail.com>
2025-03-07 17:09:33 +0200

    Add basic transformer building blocks

---
+++ rl_the_spire/models/multi_headed_attention.py
@@ -0,0 +1,86 @@
+import dataclasses
+import math
+
+import torch.nn
+
+
+@dataclasses.dataclass
+class MultiHeadedAttentionConfig:
+    n_embed_in: int
+    n_embed_kqv: int
+    n_embed_out: int
+    n_heads: int
+    attn_dropout: float
+    resid_dropout: float
+    dtype: torch.dtype
+    device: str
+    init_std: float
+
+
+class MultiHeadedAttention(torch.nn.Module):
+    def __init__(self, config: MultiHeadedAttentionConfig) -> None:
+        super().__init__()
+        self.config = config
+        assert config.n_embed_kqv % config.n_heads == 0
+        self.c_attn = torch.nn.Parameter(
+            torch.zeros(
+                (config.n_embed_in, 3 * config.n_embed_kqv),
+                dtype=config.dtype,
+                device=config.device,
+                requires_grad=True,
+            )
+        )
+        self.c_attn_bias = torch.nn.Parameter(
+            torch.zeros(
+                (3 * config.n_embed_kqv,),
+                dtype=config.dtype,
+                device=config.device,
+                requires_grad=True,
+            )
+        )
+        self.c_proj = torch.nn.Parameter(
+            torch.zeros(
+                (config.n_embed_kqv, config.n_embed_out),
+                dtype=config.dtype,
+                device=config.device,
+                requires_grad=True,
+            )
+        )
+        self.attn_dropout = torch.nn.Dropout(config.attn_dropout)
+        self.resid_dropout = torch.nn.Dropout(config.resid_dropout)
+        self.head_size = config.n_embed_kqv // config.n_heads
+        self.inv_sqrt_head_size = 1.0 / math.sqrt(self.head_size)
+        self.flash = hasattr(torch.nn.functional, "scaled_dot_product_attention")
+        if not self.flash:
+            print("WARNING: flash attention not available, using regular attention")
+
+    def init_weights(self) -> None:
+        torch.nn.init.normal_(self.c_attn, std=self.config.init_std)
+        torch.nn.init.normal_(self.c_attn_bias, std=self.config.init_std)
+        torch.nn.init.normal_(self.c_proj, std=self.config.init_std)
+
+    def forward(self, x: torch.Tensor) -> torch.Tensor:
+        *B, T, C = x.shape
+        x = torch.einsum("...ti,ij->...tj", x, self.c_attn) + self.c_attn_bias
+        q, k, v = x.split(self.config.n_embed_kqv, dim=-1)  # type: ignore
+        q = q.reshape(*B, T, self.config.n_heads, -1).transpose(1, 2)  # (B, nh, T, hs)
+        k = k.reshape(*B, T, self.config.n_heads, -1).transpose(1, 2)
+        v = v.reshape(*B, T, self.config.n_heads, -1).transpose(1, 2)
+        if self.flash:
+            y = torch.nn.functional.scaled_dot_product_attention(
+                q,
+                k,
+                v,
+                attn_mask=None,
+                dropout_p=self.config.attn_dropout if self.training else 0.0,
+            )
+        else:
+            att = (q @ k.transpose(-2, -1)) * self.inv_sqrt_head_size
+            att = torch.nn.functional.softmax(att, dim=-1)
+            att = self.attn_dropout(att)
+            y = att @ v  # (B, nh, T, T) x (B, nh, T, hs) -> (B, nh, T, hs)
+        y = (
+            y.transpose(1, 2).contiguous().view(*B, T, self.config.n_embed_out)
+        )  # re-assemble all head outputs side by side
+        y = self.resid_dropout(torch.einsum("...ti,ij->...tj", (y, self.c_proj)))
+        return y
---
+++ rl_the_spire/models/transformer_block.py
@@ -0,0 +1,83 @@
+import dataclasses
+from typing import Callable, Optional
+
+import torch
+
+from rl_the_spire.models.mlp import MLP, MLPConfig
+from rl_the_spire.models.multi_headed_attention import (
+    MultiHeadedAttention,
+    MultiHeadedAttentionConfig,
+)
+
+
+@dataclasses.dataclass
+class TransformerBlockConfig:
+    n_embed: int
+    n_extra_embed: int
+    n_heads: int
+    attn_dropout: float
+    resid_dropout: float
+    dtype: torch.dtype
+    device: str
+    linear_size_multiplier: int
+    activation: Callable[[torch.Tensor], torch.Tensor]
+    mlp_dropout: float
+    init_std: float
+    ln_eps: float
+
+
+class TransformerBlock(torch.nn.Module):
+    def __init__(self, config: TransformerBlockConfig):
+        super().__init__()
+        self.config = config
+        self.ln_1 = torch.nn.LayerNorm(
+            self.config.n_embed + self.config.n_extra_embed,
+            device=self.config.device,
+            dtype=self.config.dtype,
+            eps=self.config.ln_eps,
+        )
+        self.attn_config = MultiHeadedAttentionConfig(
+            n_embed_in=self.config.n_embed + self.config.n_extra_embed,
+            n_embed_kqv=self.config.n_embed,
+            n_embed_out=self.config.n_embed,
+            n_heads=self.config.n_heads,
+            attn_dropout=self.config.attn_dropout,
+            resid_dropout=self.config.resid_dropout,
+            dtype=self.config.dtype,
+            device=self.config.device,
+            init_std=self.config.init_std,
+        )
+        self.attn = MultiHeadedAttention(self.attn_config)
+        self.ln_2 = torch.nn.LayerNorm(
+            self.config.n_embed + self.config.n_extra_embed,
+            device=self.config.device,
+            dtype=self.config.dtype,
+            eps=self.config.ln_eps,
+        )
+        self.mlp_config = MLPConfig(
+            n_in=self.config.n_embed + self.config.n_extra_embed,
+            n_out=self.config.n_embed,
+            linear_size_multiplier=self.config.linear_size_multiplier,
+            activation=self.config.activation,
+            dropout=self.config.mlp_dropout,
+            dtype=self.config.dtype,
+            device=self.config.device,
+            init_std=self.config.init_std,
+        )
+        self.mlp = MLP(self.mlp_config)
+
+    def init_weights(self) -> None:
+        self.attn.init_weights()
+        self.mlp.init_weights()
+
+    def forward(
+        self, x: torch.Tensor, extra_embed: Optional[torch.Tensor] = None
+    ) -> torch.Tensor:
+        if self.config.n_extra_embed == 0:
+            x = x + self.attn(self.ln_1(x))
+            x = x + self.mlp(self.ln_2(x))
+        else:
+            assert extra_embed is not None
+            x = x + self.attn(self.ln_1(torch.cat([x, extra_embed], dim=-1)))
+            x = x + self.mlp(self.ln_2(torch.cat([x, extra_embed], dim=-1)))
+        return x
