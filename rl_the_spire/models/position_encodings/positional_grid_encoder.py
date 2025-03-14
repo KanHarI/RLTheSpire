Fix gradients

rl_the_spire/models/position_encodings/positional_grid_encoder.py
@@ -38,13 +38,21 @@ class PositionalGridEncoder(torch.nn.Module):
         # Create learnable row and column embeddings
         self.row_embeddings = torch.nn.Parameter(
             torch.zeros(
-                config.n_rows, config.n_embed, device=config.device, dtype=config.dtype
-            )
+                config.n_rows,
+                config.n_embed,
+                device=config.device,
+                dtype=config.dtype,
+            ),
+            requires_grad=True,
         )
         self.col_embeddings = torch.nn.Parameter(
             torch.zeros(
-                config.n_cols, config.n_embed, device=config.device, dtype=config.dtype
-            )
+                config.n_cols,
+                config.n_embed,
+                device=config.device,
+                dtype=config.dtype,
+            ),
+            requires_grad=True,
         )
 
     def init_weights(self) -> None:

rl_the_spire/models/transformers/attention_to_tensor.py
@@ -70,16 +70,16 @@ class AttentionToTensor(nn.Module):
                 (self.config.n_embed, self.config.n_output_embed * 2),
                 dtype=self.config.dtype,
                 device=self.config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         self.transformer_to_kv_bias = nn.Parameter(
             torch.zeros(
                 (self.config.n_output_embed * 2,),
                 dtype=self.config.dtype,
                 device=self.config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         # Learn two query tensors.
         # row_query: (n_output_rows, n_output_embed // 2)
@@ -88,8 +88,8 @@ class AttentionToTensor(nn.Module):
                 (self.config.n_output_rows, self.config.n_output_embed // 2),
                 dtype=self.config.dtype,
                 device=self.config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         # col_query: (n_output_columns, n_output_embed // 2)
         self.col_query = nn.Parameter(
@@ -97,8 +97,8 @@ class AttentionToTensor(nn.Module):
                 (self.config.n_output_columns, self.config.n_output_embed // 2),
                 dtype=self.config.dtype,
                 device=self.config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         # Learned linear projection to map the concatenated query to the proper query space.
         self.query_projection = nn.Parameter(
@@ -106,8 +106,8 @@ class AttentionToTensor(nn.Module):
                 (self.config.n_output_embed, self.config.n_output_embed),
                 dtype=self.config.dtype,
                 device=self.config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         # MLP for post-processing the aggregated grid.
         self.mlp_config = MLPConfig(

rl_the_spire/models/transformers/attention_to_vec.py
@@ -37,16 +37,16 @@ class AttentionToVec(torch.nn.Module):
                 (self.config.n_embed, self.config.n_output_dim * 2),
                 dtype=self.config.dtype,
                 device=self.config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         self.transformer_to_kv_bias = torch.nn.Parameter(
             torch.zeros(
                 (self.config.n_output_dim * 2,),
                 dtype=self.config.dtype,
                 device=self.config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         self.query = torch.nn.Parameter(
             torch.zeros(
@@ -56,8 +56,8 @@ class AttentionToVec(torch.nn.Module):
                 ),
                 dtype=self.config.dtype,
                 device=self.config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         self.mlp_config = MLPConfig(
             n_in=self.config.n_output_dim,

rl_the_spire/models/transformers/grid_to_sequence.py
@@ -77,24 +77,24 @@ class GridToSequence(torch.nn.Module):
                 (config.grid_n_embed, config.seq_n_embed * 2),
                 dtype=config.dtype,
                 device=config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         self.grid_to_kv_proj_bias = torch.nn.Parameter(
             torch.zeros(
                 (config.seq_n_embed * 2,),
                 dtype=config.dtype,
                 device=config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         self.c_proj = torch.nn.Parameter(
             torch.zeros(
                 (config.seq_n_embed, config.seq_n_embed),
                 dtype=config.dtype,
                 device=config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         self.attn_dropout = torch.nn.Dropout(config.attn_dropout)
         self.resid_dropout = torch.nn.Dropout(config.resid_dropout)

rl_the_spire/models/transformers/multi_headed_attention.py
@@ -30,24 +30,24 @@ class MultiHeadedAttention(torch.nn.Module):
                 (config.n_embed_in, 3 * config.n_embed_kqv),
                 dtype=config.dtype,
                 device=config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         self.c_attn_bias = torch.nn.Parameter(
             torch.zeros(
                 (3 * config.n_embed_kqv,),
                 dtype=config.dtype,
                 device=config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         self.c_proj = torch.nn.Parameter(
             torch.zeros(
                 (config.n_embed_kqv, config.n_embed_out),
                 dtype=config.dtype,
                 device=config.device,
-                requires_grad=True,
-            )
+            ),
+            requires_grad=True,
         )
         self.attn_dropout = torch.nn.Dropout(config.attn_dropout)
         self.resid_dropout = torch.nn.Dropout(config.resid_dropout)