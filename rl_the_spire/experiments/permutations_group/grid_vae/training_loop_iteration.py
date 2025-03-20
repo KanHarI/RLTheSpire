3d58414b1354431bada0eebfd9af93498cc001b4
KanHar <knaan.harpaz@gmail.com>
2025-03-20 19:00:52 +0200

    Remove unneeded dims

--- rl_the_spire/experiments/permutations_group/grid_vae/training_loop_iteration.py
+++ rl_the_spire/experiments/permutations_group/grid_vae/training_loop_iteration.py
@@ -166,10 +166,7 @@ def training_loop_iteration(
 
     all_targets = torch.stack([te_perm_mus, te_inv_mus, te_p_mus, te_q_mus, te_r_mus])
     live_to_target_l2 = torch.norm(
-        live_to_target_adapter(
-            positional_grid_encoder(denoiser_dim_expander(all_samples))
-        )
-        - all_targets,
+        live_to_target_adapter(all_samples) - all_targets,
         p=2,
         dim=(2, 3, 4),
     ).mean()
@@ -206,18 +203,12 @@ def training_loop_iteration(
 
     # Construct l2 loss to target network inv, r
     target_inv_l2 = torch.norm(
-        live_to_target_adapter(
-            positional_grid_encoder(denoiser_dim_expander(neural_inv_perm))
-        )
-        - te_inv_mus,
+        live_to_target_adapter(neural_inv_perm) - te_inv_mus,
         p=2,
         dim=(1, 2, 3),
     ).mean()
     target_comp_l2 = torch.norm(
-        live_to_target_adapter(
-            positional_grid_encoder(denoiser_dim_expander(neural_comp_perm))
-        )
-        - te_r_mus,
+        live_to_target_adapter(neural_comp_perm) - te_r_mus,
         p=2,
         dim=(1, 2, 3),
     ).mean()
