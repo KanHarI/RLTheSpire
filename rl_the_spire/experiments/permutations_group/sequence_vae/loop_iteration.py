b8690772b2eb34f61d8b0505f5b527f80a80fb2e
KanHar <knaan.harpaz@gmail.com>
2025-03-21 14:31:20 +0200

    WIP

--- rl_the_spire/experiments/permutations_group/sequence_vae/loop_iteration.py
+++ rl_the_spire/experiments/permutations_group/sequence_vae/loop_iteration.py
@@ -1,10 +1,16 @@
 import dataclasses
+from collections.abc import Iterator
+from typing import Tuple
 
 import torch
 
 
 @dataclasses.dataclass
 class LoopIterationInput:
+    dataloaders: Tuple[
+        Iterator[Tuple[torch.Tensor, torch.Tensor]],
+        Iterator[Tuple[torch.Tensor, torch.Tensor, torch.Tensor]],
+    ]
     device: torch.device
     dtype: torch.dtype
 
@@ -16,5 +22,17 @@ class LoopIterationOutput:
 
 def loop_iteration(input: LoopIterationInput) -> LoopIterationOutput:
     kl_loss = torch.tensor(0.0, device=input.device, dtype=input.dtype)
+
+    inv_dataloader, comp_dataloader = input.dataloaders
+
+    perm, inv = next(inv_dataloader)
+    p, q, r = next(comp_dataloader)
+
+    # Load to device
+    perm = perm.to(input.device)
+    inv = inv.to(input.device)
+    p = p.to(input.device)
+    q = q.to(input.device)
+    r = r.to(input.device)
 
     return LoopIterationOutput(kl_loss=kl_loss)
